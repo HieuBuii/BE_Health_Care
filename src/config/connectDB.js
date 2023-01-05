2cff97ff1732c1bdecc1a70721c1a610fdf2cf7d
Hieu Bui <hieusevenxb@gmail.com>
2023-01-05 16:27:54 +0700

    upload code

---
+++ src/config/connectDB.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER_NAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    logging: false,
+  }
+);
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+module.exports = connectDB;
